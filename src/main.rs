d219656dbcceee1946435e4b5e609a771c340dd6
Christopher Leggett <chris@leggett.dev>
2021-02-17 00:10:48 -0500

    Restructures commands and adds route for single project.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,60 @@
 use structopt::StructOpt;
 use oxidoist_api::TodoistAPI;
-use oxidoist_api::Project;
+use oxidoist_api::Project as ApiProject;
 use oxidoist_api::TodoistAPIError;
 
 use std::env;
 
 #[derive(StructOpt, Debug)]
-struct Cli {
-    verb: String, //get, add, complete, etc.
-    datatype: String, //project, task, section, etc.
+enum Cli {
+    Get {
+        #[structopt(subcommand)]
+        category: Category,
+    }
+}
+
+#[derive(StructOpt, Debug)]
+enum Category {
+    Projects(Projects),
+    Project(Project),
+}
+
+#[derive(StructOpt, Debug)]
+struct Projects {
+
+}
+
+#[derive(StructOpt, Debug)]
+struct Project {
+    id: u32,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), TodoistAPIError> {
     let args = Cli::from_args();
+    println!("{:#?}", args);
     let token = env::var("TODOIST_API_KEY").unwrap();
     let todoist_api_object = TodoistAPI::new(token.as_str()).unwrap();
-    if args.verb == "get" {
+    match args {
+        Cli::Get { category } => {
+            match category {
+                Category::Projects(_) => {
+                    let projects: Vec<ApiProject> = todoist_api_object.get_projects().await?;
+                    println!("{:#?}", projects);
+                }
+                Category::Project(project) => {
+                    let project: ApiProject = todoist_api_object.get_project(project.id).await?;
+                    println!("{:#?}", project);
+                }
+            }
+        }
+    }
+    /* if args.verb == "get" {
         if args.datatype == "projects" {
             let projects: Vec<Project> = todoist_api_object.get_projects().await?;
             println!("{:#?}", projects);
         }
-    }
+    } */
     
     Ok(())
 }
